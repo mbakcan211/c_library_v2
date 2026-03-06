6a47a1b3a1ba456b4ae60d6f568e8e169258898e
mbakcan211 <mustafa.akcan.b8@gmail.com>
2026-03-06 07:55:17 +0300

    Update MAVLink XML hashes and build dates across multiple modules
    - Changed MAVLINK_PRIMARY_XML_HASH in mavlink.h files for various modules to new hash values.
    - Updated MAVLINK_BUILD_DATE to "Fri Mar 06 2026" in version.h files for consistency.
    - Adjusted MAVLINK_XML_HASH values in specific headers to reflect new definitions.

--- ASLUAV/ASLUAV.h
+++ ASLUAV/ASLUAV.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ASLUAV.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ASLUAV_XML_HASH -6753797703213956155
+#define MAVLINK_ASLUAV_XML_HASH 8142070514713542148
 
 #ifdef __cplusplus
 extern "C" {
